7bfa09d5591a8a1bf092cb9cd506f08fee52aa95
Alex de Carvalho <leco_123carvalho@hotmail.com>
2021-05-27 00:28:28 -0300

    Initial commit

---
+++ src/main/java/com/webstore/carvalho/model/Produto.java
@@ -0,0 +1,45 @@
+package com.webstore.carvalho.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Table(name = "produtos")
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
---
+++ src/main/java/com/webstore/carvalho/testes/CadastroProdutos.java
@@ -0,0 +1,24 @@
+package com.webstore.carvalho.testes;
+
+import com.webstore.carvalho.model.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroProdutos {
+
+    public static void main(String[] args) {
+        Produto produto = new Produto();
+        produto.setNome("Celuar Xaiomi MI 9");
+        produto.setDescricao(" Ótimo celular aqui vai a descrição do produto");
+        produto.setPreco(new BigDecimal(800));
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("webstorecarvalho");
+        EntityManager  em = factory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(produto);
+        em.getTransaction().commit();
+    }
+}
