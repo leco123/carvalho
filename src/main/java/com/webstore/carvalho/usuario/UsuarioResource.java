284b5369dbd3102582b5754e129b05f35e80a5f3
Alex de Carvalho <leco_123carvalho@hotmail.com>
2021-06-03 21:19:51 -0300

    Implementado mais algumas funcionalidades

--- src/main/java/com/webstore/carvalho/usuario/UsuarioResource.java
+++ src/main/java/com/webstore/carvalho/usuario/UsuarioResource.java
@@ -3,10 +3,7 @@ package com.webstore.carvalho.usuario;
 import com.webstore.carvalho.usuario.model.Usuario;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-
 import javax.annotation.security.PermitAll;
-import javax.persistence.SequenceGenerator;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -18,16 +15,16 @@ public class UsuarioResource {
 
     @ApiModelProperty("Inclusão de Usuário")
     @POST
-    @PermitAll
-    @Transactional
-    @Consumes(MediaType.APPLICATION_JSON)
+    //@PermitAll
+    //@Transactional
+    //@Consumes(MediaType.APPLICATION_JSON)
     public void inserir(Usuario usuario) {
         // Usuario.adicionar(usuario);
     }
 
     @ApiModelProperty("Lista de Usuários")
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    //@Produces(MediaType.APPLICATION_JSON)
     public void listar(){
         //return Usuario.listAll();
     }
